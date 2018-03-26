68ff93bbfbedbbc3e34deaab833c1b07e0d7b03e
chuanwei <463266963@qq.com>
2018-03-27 01:23:30 +0800

    main最新版，修复漏报，编码问题
    Signed-off-by: chuanwei <463266963@qq.com>

---
+++ cnvdalert/readme.txt
@@ -0,0 +1,10 @@
+功能：获取cnvd最新公告，发送邮箱告警。
+
+一、0.17.1+火狐浏览器52版本+centos6.9+python2.7通过
+
+二、geckodriver放在当前目录，或者/usr/local/bin/geckodriver
+
+三、geckodriver浏览器兼容见官方说明
+https://github.com/mozilla/geckodriver/releases
+
+python main.py
---
+++ cnvdalert/setting.py
@@ -0,0 +1,21 @@
+#encoding:utf-8
+"""
+@author:Liod
+@file:setting.py
+@time:18-1-19下午4:00
+"""
+
+def adminList():
+    """
+    @函数方法:返回发送管理员的邮箱地址
+    :return: [] type list
+    """
+    # return ["2558925187@qq.com", "463266963@qq.com"]
+    return ["2558925187@qq.com", "851490083@qq.com", "1028558758@qq.com", "463266963@qq.com", "15039671626@139.com"]
+
+def userlist():
+    """
+    @函数方法:返回普通用户的邮箱地址
+    :return: [] tye list
+    """
+    return ["851490083@qq.com", "1028558758@qq.com", "463266963@qq.com", "15039671626@139.com", "13673640207@139.com"]
